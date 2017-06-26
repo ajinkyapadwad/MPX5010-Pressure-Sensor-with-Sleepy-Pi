c1935127dc92eed0b41905127c306364ba2e1c9a
Ajinkya Padwad <ajinkyapadwad@gmail.com>
2017-06-26 19:23:43 +0000

    final update

--- RunSensor.py
+++ RunSensor.py
@@ -1,59 +1,51 @@
 """
-Simple program to contiually read air pressure values from MPX5010DP sensor connected to Sleepy Pi 2
-Analog read at Sleepy Pi (Almost Arduino Uno !), reads an input sample per 100 uSecs. (= 10,000 readings per second)
-
+A simple program to run MPX5010DP pressure sensor with Sleepy Pi 2
+Analog read at Sleepy Pi (on chip Arduino Uno), reads an input sample per 100 uSecs. (= 10,000 readings per second)
+Author: Ajinkya Padwad
+Connections: 
+	Sensor	USE	Sleepy Pi 2
+	pin 1	output	pin 14 (FC0)
+	pin 2	GND	GND
+	pin 3 	VDD	5V
 """
 
-#import modules:
+#import libraries
 from pyfirmata import Arduino, util
 from time import sleep
-import os, sys
-from time import sleep
+import os
 
-class RunSensor:
-	def _init_(self):
-		#set variables:
-		self.board = Arduino('/dev/ttyS0')
+#function to run the sensor
+def RunSensor():
+	print "wait for around 10 seconds..."
+	#set the board
+	board = Arduino('/dev/ttyS0')
 
-		#start a thread
-		self.it = util.Iterator(board)
-		self.it.start()
+	#print "sleeping..."
+	sleep(2) #very important.
 
-		#set Analog pin 0 as input and configure
-		self.a0 = board.get_pin('a:0:i')
+	#start a thread
+	it = util.Iterator(board)
+	it.start()
+ 	
+	#set the analog input pin on the Arduino ( Sleepy Pi )
+	a0 = board.get_pin('a:0:i')
+ 
+	try:
+		#ignore first few 'nonetype' garbage values
+		for i in range(10):
+ 	      		garbage = a0.read()
+			#print garbage
+			sleep(0.1)
 
-		#set variables to continually read and print average for better accuracy
-		self.maxVal = 10000
-		self.valAvg = 0
-		self.finalVal = 0
-		self.valArray = []
-		self.valNum=0
+		#continually read values
+    		while True:
+			v = a0.read()*1000
+			sleep(0.5)
+        		print "Pressure: ", v, " kPa"
+    		
+	except KeyboardInterrupt:
+    		board.exit()
+    		os._exit()
 
-		#Start sensing
-		ReadValues()
-
-	def ReadValues(self):
-		try:
-			while True:
-				self.valSum = 0
-				for self.valNum in range(self.maxVal):
-					self.val = self.a0.read()
-					self.val=self.val*1000
-					self.valSum = self.valSum + self.val
-				self.valAvg = self.valSum / self.maxVal
-				# print "Avg : ", self.valAvg           
-				print "Pressure:", self.valAvg
-							
-		except Exception as e:
-			print "python exception : ",e
-			self.board.exit()
-			self.os._exit()
-
-		sys.exit(0)
-
-def main():
-	run = RunSensor()
-	sys.exit(0)
-
-if __name__ == "__main__":
-	main()
+#it all starts here !
+RunSensor()
