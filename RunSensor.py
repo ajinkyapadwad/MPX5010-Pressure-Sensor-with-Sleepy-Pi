93d13f217b99d596895d45f78b89c9b47117c670
Ajinkya Padwad <ajinkyapadwad@gmail.com>
2017-06-26 08:20:42 -0400

    latest

--- RunSensor.py
+++ RunSensor.py
@@ -1,39 +1,59 @@
 """
 Simple program to contiually read air pressure values from MPX5010DP sensor connected to Sleepy Pi 2
-Connection :
+Analog read at Sleepy Pi (Almost Arduino Uno !), reads an input sample per 100 uSecs. (= 10,000 readings per second)
+
 """
 
 #import modules:
 from pyfirmata import Arduino, util
 from time import sleep
 import os
+from time import sleep
 
-#set variables:
-board = Arduino('/dev/ttyS0')
+class RunSensor:
+	def _init_(self):
+		#set variables:
+		self.board = Arduino('/dev/ttyS0')
 
-it = util.Iterator(board)
-it.start()
+		#start a thread
+		self.it = util.Iterator(board)
+		self.it.start()
 
-a0 = board.get_pin('a:0:i')
-maxVal = 10000
+		#set Analog pin 0 as input and configure
+		self.a0 = board.get_pin('a:0:i')
 
-valAvg = 0
-finalVal = 0
-valArray = []
-sleep(3)
-valNum=0
-try:
-    while True:
-        valSum = 0
-        for valNum in range(maxVal):
-            val = a0.read()
-            val=val*1000
-            valSum = valSum + val
-        valAvg = valSum / maxVal
-        print "Avg : ", valAvg           
-        
-    
-except Exception as e:
-    print "python exception : ",e
-    board.exit()
-    os._exit()
+		#set variables to continually read and print average for better accuracy
+		self.maxVal = 10000
+		self.valAvg = 0
+		self.finalVal = 0
+		self.valArray = []
+		self.valNum=0
+
+		#Start sensing
+		ReadValues()
+
+	def ReadValues(self):
+		try:
+			while True:
+				self.valSum = 0
+				for self.valNum in range(self.maxVal):
+					self.val = self.a0.read()
+					self.val=self.val*1000
+					self.valSum = self.valSum + self.val
+				self.valAvg = self.valSum / self.maxVal
+				# print "Avg : ", self.valAvg           
+				print "Pressure:", self.valAvg
+							
+		except Exception as e:
+			print "python exception : ",e
+			self.board.exit()
+			self.os._exit()
+
+		sys.exit(0)
+
+def main():
+	run = RunSensor()
+	sys.exit(0)
+
+if __name__ == "__main__":
+	main()
